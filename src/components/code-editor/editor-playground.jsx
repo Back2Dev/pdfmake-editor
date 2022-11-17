52247119b4b9533d047d754f0b8e6b983d0519dc
Yun-Li <52901432+Sam-YunLi@users.noreply.github.com>
2022-11-17 11:48:53 +1100

    add the mini-width form split

--- src/components/code-editor/editor-playground.jsx
+++ src/components/code-editor/editor-playground.jsx
@@ -160,7 +160,18 @@ const EditorPlayground = () => {
     <>
       <Grid container className="main-area">
         <Box sx={{ height: 1 }} width="100vw">
-          <Split sx={{ height: 1 }} className="split">
+          <Split
+            className="split"
+            sizes={[50, 50]}
+            minSize={400}
+            expandToMin={false}
+            gutterSize={8}
+            gutterAlign="center"
+            snapOffset={30}
+            dragInterval={1}
+            direction="horizontal"
+            cursor="col-resize"
+          >
             <Grid sx={{ height: 1 }} item>
               <Box
                 sx={{
